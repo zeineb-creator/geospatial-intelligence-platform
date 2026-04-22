f3d0c397e2de12ddf665c8c659c9e34c88d5c2c5
Zeineb Sinini <131373163+zeineb-creator@users.noreply.github.com>
2026-04-22 12:51:53 +0100

    Add numpy import to llm_engine.py

--- geospatial_platform/llm_engine.py
+++ geospatial_platform/llm_engine.py
@@ -1,6 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import numpy as np
 from geospatial_platform.validator import validate_inputs, build_reliability_report
 from groq import Groq
 from geospatial_platform.context import InputContext
